c8a7f89a8dbc74a974b72adcef6fa191306cf6db
Arqu3 <ludwiggustavsson3@gmail.com>
2016-12-26 23:45:43 +0100

    Added basic online-multiplayer functionality

---
+++ MultiplayerGame/Assets/Scripts/ControllerPlayer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class ControllerPlayer : NetworkBehaviour
+{
+    //Public vars
+    public float m_MovementSpeed = 3.0f;
+    public float m_TurnSpeed = 150.0f;
+
+	void Start()
+    {
+	}
+	
+	void Update()
+    {
+        //Make sure only localplayer can access its own script
+        if (!isLocalPlayer)
+            return;
+
+        transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal") * m_TurnSpeed * Time.deltaTime, 0));
+        transform.Translate(new Vector3(0, 0, Input.GetAxis("Vertical") * m_MovementSpeed * Time.deltaTime));
+	}
+
+    public override void OnStartLocalPlayer()
+    {
+        if (GetComponent<MeshRenderer>())
+            GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+}
